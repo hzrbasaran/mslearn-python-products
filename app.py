5ecc012b5c31d6bcfba823cc1d95737bfa4ae6f5
hzrbasaran <hzrbasaran@hotmail.com>
2024-10-05 10:54:45 +0000

    testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 import json
 
@@ -10,6 +10,25 @@ def index():
         data = json.load(f)
 
     return render_template('index.html', products=data)
+
+@app.route("/product")
+def product_detail():
+    with open('data/products.json') as f:
+        data = json.load(f)
+
+    productId = request.args.get('q')
+    
+    productItem = list(filter(
+        lambda x: x["id"]==productId,
+        data
+    ))
+
+    print(productItem)
+
+    if len(productItem)<1:
+        return "Product not found"
+    else:
+        return render_template('product.html', product=productItem[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
